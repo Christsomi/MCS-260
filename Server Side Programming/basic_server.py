e4ba9d727262c64bb4107e5acfacffbfbd66b661
Christina Somik <somikchristina@gmail.com>
2024-04-01 12:44:59 -0500

    Test

---
+++ Server Side Programming/basic_server.py
@@ -0,0 +1,18 @@
+import http.server as hs
+
+class Server(hs.BaseHTTPRequestHandler):
+  def do_GET(self):
+    try:
+      file = open("example.html", "r")
+      html = file.read()
+      self.send_response(200)
+
+    except: 
+      html = "File not Found"
+      self.send_response(404)
+
+    self.end_headers()
+    self.wfile.write(bytes(html, 'utf-8'))
+
+server = hs.HTTPServer(('localhost', 8000), Server)
+server.serve_forever()
