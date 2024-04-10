0f784099d06ffebb96f35243c555ab0ec92b231d
Christina Somik <somikchristina@gmail.com>
2024-04-10 14:03:31 -0500

    New

---
+++ Bubble Sort/Bubble_Sort.py
@@ -0,0 +1,34 @@
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+L = [2,7,3,5,8,1]
+# [2,7,3,5,8,1] - compares 2 and 7
+# compares 3 and 7 - swaps
+# [2,3,7,5,8,1] - compares 7 and 5
+# swaps
+# [2,3,5,7,8,1] - conpare 8 and 1 continue until 1 is before 2
+# [1,2,3,5,7,8]
+
+M = [random.randint(0,100) for x in range(10)]
+
+def bubble_sort(M):
+    n = len(M)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if M[j] > M[j + 1]:
+                M[j], M[j + 1] = M[j + 1], M[j]
+            yield
+bubble_sort(M)
+print(M)
+
+def make_animation(M,sort):
+    fig =plt.figure()
+    bar = plt.bar([], [])
+    it = sort(L)
+
+    anim = animation.FuncAnimation(fig, animate, frames = 200, interval = 100)
+    plt.plot()
+
+#def animate():
+    #next(it)
